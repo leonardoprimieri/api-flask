4013b95c757fa6ab8a602a15963793b235f7a307
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2025-08-17 17:19:34 -0300

    feat: get task by id

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ tasks = []
 @app.route('/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
-    new_task = Task(id=uuid.uuid4(),title=data['title'], description=data.get('description', ''))
+    new_task = Task(id=str(uuid.uuid4()),title=data['title'], description=data.get('description', ''))
     tasks.append(new_task)
     return jsonify({
-        "message": 'Nova tarefa criada com sucesso!'
+        "message": 'Task successfully created!'
     })
 
 @app.route('/tasks', methods=['GET'])
@@ -22,6 +22,16 @@ def get_tasks():
         "tasks": task_list,
         "total_tasks": len(task_list)
     })
+
+@app.route('/tasks/<string:id>',methods=['GET'])
+def get_task_by_id(id):
+    for task in tasks:
+        if task.id == id:
+            return jsonify(task.to_dict())
+        
+    return jsonify({
+            "message": "No task found."
+        }), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
