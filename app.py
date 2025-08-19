a77a5203acb130a70574fa0ae737677af8a0833e
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2025-08-18 23:43:44 -0300

    feat: update task

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify
 from models.task import Task
-import uuid
 
 app = Flask(__name__)
 
+task_control_id = 1
 tasks = []
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
+    global task_control_id
     data = request.get_json()
-    new_task = Task(id=str(uuid.uuid4()),title=data['title'], description=data.get('description', ''))
+    new_task = Task(id=task_control_id,title=data['title'], description=data.get('description', ''))
     tasks.append(new_task)
+    task_control_id += 1
     return jsonify({
         "message": 'Task successfully created!'
     })
@@ -23,15 +25,29 @@ def get_tasks():
         "total_tasks": len(task_list)
     })
 
-@app.route('/tasks/<string:id>',methods=['GET'])
-def get_task_by_id(id):
+@app.route('/tasks/<int:task_id>',methods=['GET'])
+def get_task_by_id(task_id):
     for task in tasks:
-        if task.id == id:
+        if task.id == task_id:
             return jsonify(task.to_dict())
         
     return jsonify({
             "message": "No task found."
         }), 404
 
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task_by_id(task_id):
+    data = request.get_json()
+    for task in tasks:
+        if task.id == task_id:
+            task.title = data.get('title', task.title)
+            task.is_completed = data.get('is_completed', task.is_completed)
+            task.description = data.get('description', task.description)
+            return jsonify({"message": "Task successfully updated."})
+        
+    return jsonify({
+        "message": "No task found."
+    }), 404 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
