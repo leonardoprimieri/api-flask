81205ebf22a5be80e81b194641326b9464dce02a
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2025-08-19 09:52:21 -0300

    Delete task and improve methods

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def find_task_by_id(task_id):
     for task in tasks:
         if task.id == task_id:
             return task
+    return None
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
@@ -34,9 +35,7 @@ def get_tasks():
 def get_task_by_id(task_id):
     task = find_task_by_id(task_id=task_id)
     if task:
-        for task in tasks:
-            if task.id == task_id:
-                return jsonify(task.to_dict())
+        return jsonify(task.to_dict())
         
     return jsonify({
             "message": "No task found."
@@ -47,11 +46,10 @@ def update_task_by_id(task_id):
     data = request.get_json()
     task = find_task_by_id(task_id=task_id)
     if task:
-        if task.id == task_id:
-            task.title = data.get('title', task.title)
-            task.is_completed = data.get('is_completed', task.is_completed)
-            task.description = data.get('description', task.description)
-            return jsonify({"message": "Task successfully updated."})
+        task.title = data.get('title', task.title)
+        task.is_completed = data.get('is_completed', task.is_completed)
+        task.description = data.get('description', task.description)
+        return jsonify({"message": "Task successfully updated."})
         
     return jsonify({
         "message": "No task found."
